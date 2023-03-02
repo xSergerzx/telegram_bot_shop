313e6aa065a110211e76cd0b68f5386faaae21a8
xSergerzx <60799615+xSergerzx@users.noreply.github.com>
2023-03-02 21:26:45 +0200

    1

--- handlers/other.py
+++ handlers/other.py
@@ -5,7 +5,6 @@ import json
 import string
 
 
-# @dp.message_handler()
 async def echo_send(message: types.Message):
     if {i.lower().translate(str.maketrans("", "", string.punctuation)) for i in message.text.split(" ")}\
             .intersection(set(json.load(open("cenz.json")))) != set():
